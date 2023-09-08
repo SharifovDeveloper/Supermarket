82621c4c2505f204cf3b37963841ff2e6b67ce70
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-09-08 10:54:15 +0500

    initial commit

---
+++ Supermarket/Supermarket/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DAL dal = new DAL();
+            dal.CreateProduct("Fanta",1200);
+            dal.ReadAllProducts();
+        }
+    }
+}
